f9e8984a64b3fcd2d92df000fb082c0d5762b923
BPRoberts1988 <roberts.brianp@gmail.com>
2024-05-01 11:15:58 -0700

    Committing completed dictionary

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    num_letters = get_num_letters(num_words)
+    letter_count = get_num_letters(text)
     print(f"{num_words} words found in the document")
+    print(letter_count)
 
 
 def get_num_words(text):
@@ -14,10 +15,17 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-    
-def get_num_letters(num_letters):
-    print(num_letters)
 
-    
+
+def get_num_letters(letter_count):
+    letter_dictionary = {}
+    for i in letter_count:
+        lowercase = i.lower()
+        if lowercase in letter_dictionary:
+            letter_dictionary[lowercase] += 1
+        else:
+            letter_dictionary[lowercase] = 1
+    return letter_dictionary
+
 
 main()
