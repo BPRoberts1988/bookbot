61ad3f1d81ac0fd4b368219cc98c820637ec2eb2
BPRoberts1988 <roberts.brianp@gmail.com>
2024-05-04 12:40:59 -0700

    Completed Book Bot Commit

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    letter_count = get_num_letters(text)
+    sorted_dict_list = get_num_letters(text)
+    print(f"--- Begin Report of {book_path} ---")
+    print("")
     print(f"{num_words} words found in the document")
-    print(letter_count)
+    print("")
+    for item in sorted_dict_list:
+        print(f"Character {item['Letter']} was found {item['Count']} times!!!")
+    print("")
+    print("--- End Report ---")
 
 
 def get_num_words(text):
@@ -20,12 +26,15 @@ def get_book_text(path):
 def get_num_letters(letter_count):
     letter_dictionary = {}
     for i in letter_count:
-        lowercase = i.lower()
+        if i.isalpha():
+            lowercase = i.lower()
         if lowercase in letter_dictionary:
             letter_dictionary[lowercase] += 1
         else:
             letter_dictionary[lowercase] = 1
-    return letter_dictionary
-
+    list_of_dict = [{"Letter": key, "Count": value} for key, value in letter_dictionary.items()]
+    sorted_dict_list = sorted(list_of_dict, key = lambda x: x["Count"], reverse = True)
+    return sorted_dict_list
+    
 
 main()
